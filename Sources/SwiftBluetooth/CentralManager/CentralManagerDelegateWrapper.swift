826d5683178e680b6091edd9c0f80c968b034697
Aaron Taylor <aaron@exphat.com>
2023-12-29 10:45:39 -0500

    Reintroduce event queueing for central manager delegate wrapper

--- Sources/SwiftBluetooth/CentralManager/CentralManagerDelegateWrapper.swift
+++ Sources/SwiftBluetooth/CentralManager/CentralManagerDelegateWrapper.swift
@@ -11,38 +11,45 @@ class CentralManagerDelegateWrapper: NSObject, CBCentralManagerDelegate {
     // MARK: - CBCentralManagerDelegate conformance
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         guard let parent = self.parent else { return }
-
-        parent.eventSubscriptions.recieve(.stateUpdated(parent.state))
-        parent.delegate?.centralManagerDidUpdateState(parent)
+        parent.eventQueue.async {
+            parent.eventSubscriptions.recieve(.stateUpdated(parent.state))
+            parent.delegate?.centralManagerDidUpdateState(parent)
+        }
     }
 
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         guard let parent = parent else { return }
         let peripheral = parent.peripheral(peripheral)
 
-        parent.connectedPeripherals.insert(peripheral)
-        parent.eventSubscriptions.recieve(.connected(peripheral))
-        parent.delegate?.centralManager(parent, didConnect: peripheral)
+        parent.eventQueue.async {
+            parent.connectedPeripherals.insert(peripheral)
+            parent.eventSubscriptions.recieve(.connected(peripheral))
+            parent.delegate?.centralManager(parent, didConnect: peripheral)
+        }
     }
 
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
         guard let parent = parent else { return }
         let peripheral = parent.peripheral(peripheral)
 
-        parent.connectedPeripherals.remove(peripheral)
-        parent.eventSubscriptions.recieve(.disconnected(peripheral, error))
-        peripheral.eventSubscriptions.recieve(.didDisconnect(error))
-        parent.delegate?.centralManager(parent, didDisconnectPeripheral: peripheral, error: error)
+        parent.eventQueue.async {
+            parent.connectedPeripherals.remove(peripheral)
+            parent.eventSubscriptions.recieve(.disconnected(peripheral, error))
+            peripheral.eventSubscriptions.recieve(.didDisconnect(error))
+            parent.delegate?.centralManager(parent, didDisconnectPeripheral: peripheral, error: error)
 
-        parent.removePeripheral(peripheral.cbPeripheral)
+            parent.removePeripheral(peripheral.cbPeripheral)
+        }
     }
 
     func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
         guard let parent = parent else { return }
         let peripheral = parent.peripheral(peripheral)
 
-        parent.eventSubscriptions.recieve(.failToConnect(peripheral, error))
-        parent.delegate?.centralManager(parent, didFailToConnect: peripheral, error: error)
+        parent.eventQueue.async {
+            parent.eventSubscriptions.recieve(.failToConnect(peripheral, error))
+            parent.delegate?.centralManager(parent, didFailToConnect: peripheral, error: error)
+        }
     }
 
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
@@ -50,15 +57,19 @@ class CentralManagerDelegateWrapper: NSObject, CBCentralManagerDelegate {
         let peripheral = parent.peripheral(peripheral)
         peripheral.discovery = .init(rssi: RSSI, advertisementData: advertisementData)
 
-        parent.eventSubscriptions.recieve(.discovered(peripheral, advertisementData, RSSI))
-        parent.delegate?.centralManager(parent, didDiscover: peripheral, advertisementData: advertisementData, rssi: RSSI)
+        parent.eventQueue.async {
+            parent.eventSubscriptions.recieve(.discovered(peripheral, advertisementData, RSSI))
+            parent.delegate?.centralManager(parent, didDiscover: peripheral, advertisementData: advertisementData, rssi: RSSI)
+        }
     }
 
     func centralManager(_ central: CBCentralManager, willRestoreState dict: [String: Any]) {
         guard let parent = parent else { return }
 
-        parent.eventSubscriptions.recieve(.restoreState(dict))
-        parent.delegate?.centralManager(parent, willRestoreState: dict)
+        parent.eventQueue.async {
+            parent.eventSubscriptions.recieve(.restoreState(dict))
+            parent.delegate?.centralManager(parent, willRestoreState: dict)
+        }
     }
 
     #if os(iOS)
@@ -66,14 +77,18 @@ class CentralManagerDelegateWrapper: NSObject, CBCentralManagerDelegate {
         guard let parent = parent else { return }
         let peripheral = parent.peripheral(peripheral)
 
-        parent.delegate?.centralManager(parent, connectionEventDidOccur: event, for: peripheral)
+        parent.eventQueue.async {
+            parent.delegate?.centralManager(parent, connectionEventDidOccur: event, for: peripheral)
+        }
     }
 
     func centralManager(_ central: CBCentralManager, didUpdateANCSAuthorizationFor peripheral: CBPeripheral) {
         guard let parent = parent else { return }
         let peripheral = parent.peripheral(peripheral)
 
-        parent.delegate?.centralManager(parent, didUpdateANCSAuthorizationFor: peripheral)
+        parent.eventQueue.async {
+            parent.delegate?.centralManager(parent, didUpdateANCSAuthorizationFor: peripheral)
+        }
     }
     #endif
 }
